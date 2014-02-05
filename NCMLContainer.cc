314e1b4e9293a90ff7124e013f659e59775dc6cc
James Gallagher <jgallagher@opendap.org>
2014-02-05 23:49:42 +0000

    Changes for OSX 10.9, plenty of tests still fail

--- NCMLContainer.cc
+++ NCMLContainer.cc
@@ -30,14 +30,16 @@
 // Authors:
 //      pcw       Patrick West <pwest@ucar.edu>
 
+#include <unistd.h>
+
 #include <fstream>
 #include <cerrno>
 #include <cstdlib>
 #include <cstring>
 
-using std::ofstream ;
-using std::endl ;
-using std::ios_base ;
+using std::ofstream;
+using std::endl;
+using std::ios_base;
 
 #include "NCMLContainer.h"
 #include "NCMLContainerStorage.h"
@@ -59,56 +61,46 @@ using std::ios_base ;
  * @param ncml_doc string of the xml document
  * @see NCMLGatewayUtils
  */
-NCMLContainer::NCMLContainer( const string &sym_name, const string &xml_doc )
-    : BESContainer( sym_name, "", "ncml" ),
-      _xml_doc( xml_doc ),
-      _accessed( false )
+NCMLContainer::NCMLContainer(const string &sym_name, const string &xml_doc) :
+		BESContainer(sym_name, "", "ncml"), _xml_doc(xml_doc), _accessed(false)
 {
 }
 
-NCMLContainer::NCMLContainer( const NCMLContainer &copy_from )
-    : BESContainer( copy_from ),
-      _xml_doc( copy_from._xml_doc ),
-      _accessed( copy_from._accessed )
+NCMLContainer::NCMLContainer(const NCMLContainer &copy_from) :
+		BESContainer(copy_from), _xml_doc(copy_from._xml_doc), _accessed(copy_from._accessed)
 {
-    // we can not make a copy of this container once the NCML document has
-    // been written to the temporary file
-    if( _accessed )
-    {
-	string err = (string)"The Container has already been accessed, "
-	             + "can not create a copy of this container." ;
-	throw BESInternalError( err, __FILE__, __LINE__ ) ;
-    }
+	// we can not make a copy of this container once the NCML document has
+	// been written to the temporary file
+	if (_accessed) {
+		string err = (string) "The Container has already been accessed, " + "can not create a copy of this container.";
+		throw BESInternalError(err, __FILE__, __LINE__);
+	}
 }
 
-void
-NCMLContainer::_duplicate( NCMLContainer &copy_to )
+void NCMLContainer::_duplicate(NCMLContainer &copy_to)
 {
-    if( copy_to._accessed )
-    {
-	string err = (string)"The Container has already been accessed, "
-	             + "can not duplicate this resource." ;
-	throw BESInternalError( err, __FILE__, __LINE__ ) ;
-    }
-    copy_to._xml_doc = _xml_doc ;
-    copy_to._accessed = false ;
-    BESContainer::_duplicate( copy_to ) ;
+	if (copy_to._accessed) {
+		string err = (string) "The Container has already been accessed, " + "can not duplicate this resource.";
+		throw BESInternalError(err, __FILE__, __LINE__);
+	}
+	copy_to._xml_doc = _xml_doc;
+	copy_to._accessed = false;
+	BESContainer::_duplicate(copy_to);
 }
 
 BESContainer *
-NCMLContainer::ptr_duplicate( )
+NCMLContainer::ptr_duplicate()
 {
-    NCMLContainer *container = new NCMLContainer ;
-    _duplicate( *container ) ;
-    return container ;
+	NCMLContainer *container = new NCMLContainer;
+	_duplicate(*container);
+	return container;
 }
 
 NCMLContainer::~NCMLContainer()
 {
-    if( _accessed )
-    {
-	release() ;
-    }
+	if (_accessed) {
+		release();
+	}
 }
 
 /** @brief access the NCML target response by making the NCML request
@@ -116,66 +108,57 @@ NCMLContainer::~NCMLContainer()
  * @return full path to the NCML request response data file
  * @throws BESError if there is a problem making the NCML request
  */
-string
-NCMLContainer::access()
+string NCMLContainer::access()
 {
-    BESDEBUG( "ncml", "accessing " << _xml_doc << endl );
-    if( !_accessed )
-    {
-	// save the xml document to a temporary file, open it, unlink
-	// it. In release, close the file. This will remove the file if
-	// it is no longer open.
+	BESDEBUG("ncml", "accessing " << _xml_doc << endl);
+	if (!_accessed) {
+		// save the xml document to a temporary file, open it, unlink
+		// it. In release, close the file. This will remove the file if
+		// it is no longer open.
 
-	string tempfile_template = "ncml_module_XXXXXX" ;
+		string tempfile_template = "ncml_module_XXXXXX";
 #if defined(WIN32) || defined(TEST_WIN32_TEMPS)
-	char *tempfile_c = _mktemp( (char *)tempfile_template.c_str() ) ;
+		char *tempfile_c = _mktemp( (char *)tempfile_template.c_str() );
 #else
-	char *tempfile_c = mktemp( (char *)tempfile_template.c_str() ) ;
+		char *tempfile_c = mktemp((char *) tempfile_template.c_str());
 #endif
-	string tempfile ;
-	if( tempfile_c )
-	{
-	    tempfile = tempfile_c ;
+		string tempfile;
+		if (tempfile_c) {
+			tempfile = tempfile_c;
+		}
+		else {
+			string err = (string) "Unable to create temporary ncml document " + _tmp_file_name;
+			throw BESInternalError(err, __FILE__, __LINE__);
+		}
+
+		_tmp_file_name = NCMLContainerStorage::NCML_TempDir + "/" + tempfile + ".ncml";
+
+		ofstream ostrm;
+		int my_errno = 0;
+
+		ostrm.open(_tmp_file_name.c_str(), ios_base::out);
+		my_errno = errno;
+
+		if (!ostrm) {
+			string err = (string) "Unable to write out the ncml document " + _tmp_file_name;
+			if (my_errno) {
+				char *str = strerror(my_errno);
+				if (str) err += (string) " " + str;
+			}
+			throw BESInternalError(err, __FILE__, __LINE__);
+		}
+
+		// write out <?xml version="1.0" encoding="UTF-8"?>
+		ostrm << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl;
+
+		// then write out the r_name as the ncml document (no validation)
+		ostrm << _xml_doc << endl;
+
+		ostrm.close();
+
+		_accessed = true;
 	}
-	else
-	{
-	    string err = (string)"Unable to create temporary ncml document "
-			 + _tmp_file_name ;
-	    throw BESInternalError( err, __FILE__, __LINE__ ) ;
-	}
-
-	_tmp_file_name = NCMLContainerStorage::NCML_TempDir
-			 + "/" + tempfile + ".ncml" ;
-
-	ofstream ostrm ;
-	int my_errno = 0 ;
-
-	ostrm.open( _tmp_file_name.c_str(), ios_base::out ) ;
-	my_errno = errno ;
-
-	if( !ostrm )
-	{
-	    string err = (string)"Unable to write out the ncml document "
-			 + _tmp_file_name ;
-	    if( my_errno )
-	    {
-		char *str = strerror( my_errno ) ;
-		if( str ) err += (string)" " + str ;
-	    }
-	    throw BESInternalError( err, __FILE__, __LINE__ ) ;
-	}
-
-	// write out <?xml version="1.0" encoding="UTF-8"?>
-	ostrm << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl ;
-
-	// then write out the r_name as the ncml document (no validation)
-	ostrm << _xml_doc << endl ;
-
-	ostrm.close() ;
-
-	_accessed = true ;
-    }
-    return _tmp_file_name ;
+	return _tmp_file_name;
 }
 
 /** @brief release the NCML cached resources
@@ -184,16 +167,14 @@ NCMLContainer::access()
  *
  * @return true if the resource is released successfully and false otherwise
  */
-bool
-NCMLContainer::release()
+bool NCMLContainer::release()
 {
-    if( _accessed && !_tmp_file_name.empty() )
-    {
-	unlink( _tmp_file_name.c_str() ) ;
-	_tmp_file_name = "" ;
-    }
-    _accessed = false ;
-    return true ;
+	if (_accessed && !_tmp_file_name.empty()) {
+		unlink(_tmp_file_name.c_str());
+		_tmp_file_name = "";
+	}
+	_accessed = false;
+	return true;
 }
 
 /** @brief dumps information about this object
@@ -203,22 +184,17 @@ NCMLContainer::release()
  *
  * @param strm C++ i/o stream to dump the information to
  */
-void
-NCMLContainer::dump( ostream &strm ) const
+void NCMLContainer::dump(ostream &strm) const
 {
-    strm << BESIndent::LMarg << "NCMLContainer::dump - ("
-			     << (void *)this << ")" << endl ;
-    BESIndent::Indent() ;
-    if( _accessed )
-    {
-	strm << BESIndent::LMarg << "temporary file: "
-				 << _tmp_file_name << endl ;
-    }
-    else
-    {
-	strm << BESIndent::LMarg << "temporary file: not open" << endl ;
-    }
-    BESContainer::dump( strm ) ;
-    BESIndent::UnIndent() ;
+	strm << BESIndent::LMarg << "NCMLContainer::dump - (" << (void *) this << ")" << endl;
+	BESIndent::Indent();
+	if (_accessed) {
+		strm << BESIndent::LMarg << "temporary file: " << _tmp_file_name << endl;
+	}
+	else {
+		strm << BESIndent::LMarg << "temporary file: not open" << endl;
+	}
+	BESContainer::dump(strm);
+	BESIndent::UnIndent();
 }
 
